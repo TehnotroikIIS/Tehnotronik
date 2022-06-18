118c81ef9376315b01ec03eeae3d95712656eba5
PedjaWDS <despotovicpedja@gmail.com>
2022-06-18 14:33:36 +0200

    added domain models

---
+++ Tehnotronik/Domain/Models/StorageOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tehnotronik.Domain.Models
+{
+    public class StorageOrder
+    {
+        public Guid Id { get; set; }
+        public Guid StorageProductId { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
---
+++ Tehnotronik/Domain/Models/StorageProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tehnotronik.Domain.Models
+{
+    public class StorageProduct
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public LocationEnum Location { get; set; }
+        public int Quantity { get; set; }
+        public int AvailableQuantity { get; set; }
+        public int MinimalQuantity { get; set; }
+        public PriorityEnum Priority { get; set; }
+        public SKUEnum SKU { get; set; }
+        public int SKUCapacity { get; set; }
+    }
+}
