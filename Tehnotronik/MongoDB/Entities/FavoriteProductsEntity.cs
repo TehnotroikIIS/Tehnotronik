2bf01bc23cab5ccdeece8af556e87a708ee4a309
aleksandra.mitro <sandramitro99@gmail.com>
2022-06-18 15:45:34 +0200

    done fav products

---
+++ Tehnotronik/MongoDB/Entities/FavoriteProductsEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tehnotronik.Domain.Models;
+using Tehnotronik.MongoDB.Attributes;
+
+namespace Tehnotronik.MongoDB.Entities
+{
+    [CollectionName("FavoriteProducts")]
+    public class FavoriteProductsEntity : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid[] Products { get; set; }
+        public FavoriteProducts ToFavoriteProducts()
+            => new FavoriteProducts(this.Id, this.UserId, this.Products);
+        public static FavoriteProductsEntity ToFavoriteProductsEntity(FavoriteProducts favoriteProducts)
+        {
+            return new FavoriteProductsEntity
+            {
+                Id = favoriteProducts.Id,
+                UserId = favoriteProducts.UserId,
+                Products = favoriteProducts.Products
+            };
+        }
+    }
+}
